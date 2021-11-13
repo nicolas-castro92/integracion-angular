325c69d531f7e8f28f4e302e0a2b719f1debaddf
Nicolas Castro Giraldo <nicolas.castro9219@gmail.com>
2021-11-12 21:53:07 -0500

    personalizando background con primeng

---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// modulos
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    // exportarlos
+  ]
+})
+export class PrimeNgModule { }
