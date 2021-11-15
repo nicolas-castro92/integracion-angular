6e11a18c2a61c77fad9a124d000fff8ae99c05f1
Nicolas Castro Giraldo <nicolas.castro9219@gmail.com>
2021-11-14 19:08:27 -0500

    creacion del guard para autenticacion

--- src/app/interfaces/auth.interface.ts
+++ src/app/interfaces/auth.interface.ts
@@ -1,8 +1,9 @@
 export interface AuthResp {
-    ok:      boolean;
-    tk?:      string;
-    usuario?: Usuario;
-    mensaje?: string;
+    ok:           boolean;
+    tk?:          string;
+    usuario?:     Usuario;
+    usuarioxrol?: Usuarioxrol;
+    mensaje?:     string;
 }
 
 export interface Respuesta {
@@ -17,3 +18,10 @@ export interface Usuario {
     celular?:     string;
     estado?:      string;
 }
+
+export interface Usuarioxrol {
+    id:         number;
+    id_usuario: number;
+    id_rol:     number;
+}
+
